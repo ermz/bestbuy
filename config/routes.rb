aa9c8f85e2178493895569a6a63b2673c21d61a9
Edson Ramirez <eramirez4369@gmail.com>
2018-07-31 13:13:37 -0400

    created orders controller and landing_page view for simple pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  root 'simple_pages#landing_page'
+
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
-
-  root'simple_pages#index'
 
   get 'simple_pages/index'
 
