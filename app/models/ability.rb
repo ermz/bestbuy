7514ba472a4f3b69c7f85a328176502d50ed324b
Edson Ramirez <eramirez4369@gmail.com>
2018-09-05 02:03:53 -0400

    added admin roles with cancan and a delete button for comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,12 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    can :manage, Comment, id: user.id
+    can :read, [Comment, Order, Product]
+
+    if user.present & user.admin?
+      can :manage, :all
+    end
   end
 
 end
